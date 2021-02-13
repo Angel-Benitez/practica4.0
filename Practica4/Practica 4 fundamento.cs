dde9385b2ce6b63e704365d2da3668bfe96c03d4
Angel-Benitez <78944477+Angel-Benitez@users.noreply.github.com>
2021-02-12 23:16:04 -0400

    Add files via upload
    Angel Benitez
    Matricula: 2020-10534
    Grupo-21

---
+++ Practica4/Practica 4 fundamento.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Prac4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Crear un programa que pida números positivos al usuario, y vaya calculando 
+            //la suma de todos ellos (terminará cuando se teclea un número negativo o cero).
+            //int a, b = 0, c;
+            //do
+            //{
+            //    Console.WriteLine("Digite números positivos para sumar");
+            //    Console.WriteLine("Digite un número igual o menor que cero para terminar");
+            //    a = int.Parse(Console.ReadLine());
+            //    if (a > 0)
+            //    {
+            //        b = b + a;
+            //        c = b - a;
+            //        Console.WriteLine("{0} + {1} = {2}", c, a, b);
+            //        Console.WriteLine("--------------------------------------------------");
+            //    }
+            //} while (a! > 1);
+            //Console.Clear();
+            //Console.WriteLine("Gracias");
+            //Console.ReadKey();
+            //____________________________________________________
+
+            //2.Crea un programa que escriba en pantalla 
+            //los números del 1 al 10, usando "do..while".
+            //int a = 1;
+            //do
+            //{
+            //    Console.WriteLine(+a);
+            //    a = a + 1;
+            //} while (a != 11);
+            //Console.ReadKey();
+            //____________________________________________________
+
+            //3.Crear un programa que muestre los primeros 
+            //10 números pares a partir del producto de(10 x 10).
+            //int a = 0, b, c = 0;
+            //while (a < 10)
+            //{
+            //    b = 10 * 10 + c;
+            //    c = c + 1;
+            //    if (b % 2 == 0)
+            //    {
+            //        Console.WriteLine(+b);
+            //        a = a + 1;
+            //    }
+            //}
+            //Console.ReadKey();
+            //____________________________________________________
+
+            //4.Crear un programa que muestre las letras de 
+            //la Z(mayúscula) a la A(mayúscula, descendiendo).
+            //int inti = 0;
+            //Console.WriteLine("Abecedario decendente enmayusculas");
+            //for (inti = 90; inti >= 65; inti--)
+            //{
+            //    Console.WriteLine("Letra: " + Convert.ToString((char)inti));
+            //}
+            //Console.ReadKey();
+
+
+        }
+    }
+}
